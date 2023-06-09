2071a3bf4b573e14f68697f31ad491b4fa0f2a95
CarloBattista <carlitobatti@gmail.com>
2023-06-09 15:43:49 +0200

    collegamento databse

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        $title = 'Home';
+
+        // query database Movies
+        $arrayMovies = Movie::all();
+
+        return view('pages.home', compact('title','arrayMovies'));
+    }
 }
